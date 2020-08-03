a6903c199180eccd05f479b0cda189bd1ee98187
Carlos Rosario <carlosrosario2k@gmail.com>
2020-08-03 14:14:47 -0400

    deleted and created new rb file

--- db/migrate/20200803153953_create_bucket_fillers.rb
+++
@@ -1,9 +0,0 @@
-class CreateBucketFillers < ActiveRecord::Migration
-  def change
-    create_table :bucket_fillers do |t|
-      t.string :bucket_list
-      t.integer :user_id
-      t.timestamps null: false
-    end
-  end
-end
