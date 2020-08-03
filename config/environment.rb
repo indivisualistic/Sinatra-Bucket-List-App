5f7970c4649f0adc059260052c24fdce70be97bb
Carlos Rosario <carlosrosario2k@gmail.com>
2020-08-03 13:43:09 -0400

    added database

--- config/environment.rb
+++ config/environment.rb
@@ -5,8 +5,12 @@ Bundler.require(:default, ENV['SINATRA_ENV'])
 
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
+  :database => "db/#{ENV['SINATRA_ENV']}.sqlite")
+  
+  set database: {
+  :adapter => "sqlite3",
   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
+}
 
 require './app/controllers/application_controller'
 require_all 'app'
