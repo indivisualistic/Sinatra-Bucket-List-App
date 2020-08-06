fed156fc894e1b8b1a2fd642a3b9babc15de8628
Carlos Rosario <carlosrosario2k@gmail.com>
2020-08-06 11:30:31 -0400

    changed string name to content

--- db/migrate/002_create_bucket_fillers.rb
+++ db/migrate/002_create_bucket_fillers.rb
@@ -1,7 +1,7 @@
 class CreateBucketFillers < ActiveRecord::Migration[6.0]
   def change
     create_table :bucket_fillers do |t|
-      t.string :bucket_list
+      t.string :content
       t.integer :user_id
       t.timestamps null: false
     end
