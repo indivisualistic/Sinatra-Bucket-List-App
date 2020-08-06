c5bdf6177ba38a139312b0d4d86f59a343f3f42d
Carlos Rosario <carlosrosario2k@gmail.com>
2020-08-06 11:23:54 -0400

    refactored if params hash

--- app/controllers/bucket_fillers_controller.rb
+++ app/controllers/bucket_fillers_controller.rb
@@ -4,8 +4,32 @@ class BucketFillersController < ApplicationController
     erb :'bucket_fillers/new'
   end
 
-  post '/journal_entries' do
+  post '/bucket_fillers' do
+    #create new journal entry and save it to the database
+    #only save entry if there is content added
+    if !logged_in?
+      redirect '/'
+    end
     
+  
+#params is a hash
+    if params[:content] != ""
+      bucket_filler = current_user.bucket_fillers.build(content: params[:content])
+      if bucket_filler.save
+        redirect "/bucket_fillers/#{bucket_filler.id}"
+      
+      end
+    end
+  redirect '/bucket_fillers/new'
+   
+    #only create entry if user is logged in. 
+    
+  end
+
+  get '/bucket_fillers/:id' do
+    @bucket_filler = BucketFiller.find(params[:id])
+    # set_bucket_filler
+    erb :'/bucket_fillers/show'
   end
   #post request is made in bucket_fillers to create a new list nentry
   #show route for journal entry
